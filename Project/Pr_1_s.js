30911e5de38df620609f83df6cf19588940b38fc
Ogorodnik-Artem <artem.ok2004@gmail.com>
2025-02-08 18:58:59 +0300

    Удаление ненужных файлов

--- Project/Pr_1_s.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("1").innerText = "Hi";
-let canvas1 = document.getElementById("canvas1");
-canvas.height = 300;
-canvas.width = 300;
-let ctx = canvas1.getContex("2d");
-let img = document.getElementById("picture_1");
-ctx.drawImage(img, 100, 100);
-
-
